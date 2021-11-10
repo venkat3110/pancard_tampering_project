a647961f2452d0c20af40ef0dfb651302243c80c
Joseph Venkat <83961689+venkat3110@users.noreply.github.com>
2021-11-10 18:01:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+from os import environ
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'joseph'
+
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "joseph"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "pianalytix"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "pianalytix"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+
+class DebugConfig(Config):
+    DEBUG = False
